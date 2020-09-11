7839391bcf40c1a69bae091d9492774515b558af
chamcham3 <is0456fx@ed.ritsumei.ac.jp>
2020-09-12 01:16:10 +0900

    とりあえずフェードアウトするスクリプトを作成したのですがなんかうまく動いてないです また他を結合して後で作成します。

--- Kazehahuku/Assets/Scripts/TitleManager.cs
+++ Kazehahuku/Assets/Scripts/TitleManager.cs
@@ -6,7 +6,21 @@ using UnityEngine.EventSystems;
 
 public class TitleManager : MonoBehaviour
 {
+    // public GameObject prefab;
+    GameObject refObj;
+
+	void Start() {
+		refObj = GameObject.Find("Panel");
+	}
     public void OnStartClickd(){
+        //Instantiate(prefab, Vector3.zero, Quaternion.identity);
+        FadeScript f1 = refObj.GetComponent<FadeScript>();
+        f1.Fadeout();
+        Invoke("LoadScene", 3f);
+        LoadScene();
+    }
+
+    public void LoadScene(){
         SceneManager.LoadScene("PlayerTestScene");
     }
 }
