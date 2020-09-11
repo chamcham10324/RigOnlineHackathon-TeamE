3fdc52a26ac8e885fea46a53050d6e1259d81c9d
CODE2431 <code0229-hayate@ezweb.ne.jp>
2020-09-11 19:18:42 +0900

    add animation プレイヤーのアニメーションを付けました

--- Kazehahuku/Assets/Scripts/PlayerManager.cs
+++ Kazehahuku/Assets/Scripts/PlayerManager.cs
@@ -4,61 +4,159 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
+    [SerializeField] ResultManager resultManager;
     private Vector3 moveDirection;
     private float x;
     private float y;
     private Animator playerAnimator;
+    private Rigidbody2D rb;
+    private PlayerStatus playerStatus = PlayerStatus.LF;
+    private float clearTime;
+    private float damage;
+    private float operation;
+
+    public enum PlayerStatus
+    { 
+        RF,
+        LF,
+        RB,
+        LB
+    }
 
     // Start is called before the first frame update
     void Start()
     {
         playerAnimator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        clearTime += Time.deltaTime;
+
         // 入力に応じで速度を設定
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             x = 0;
             y = 3;
-            playerAnimator.SetBool("Back",true);
-            playerAnimator.SetBool("Forward", false);
-            playerAnimator.SetBool("Right", false);
-            playerAnimator.SetBool("Left", false);
+
+            if (playerStatus == PlayerStatus.RF)
+            {
+                playerAnimator.SetBool("RF", false);
+                playerAnimator.SetBool("LF", false);
+                playerAnimator.SetBool("RB", true);
+                playerAnimator.SetBool("LB", false);
+                playerStatus = PlayerStatus.RB;
+            }
+
+            if (playerStatus == PlayerStatus.LF)
+            {
+                playerAnimator.SetBool("RF", false);
+                playerAnimator.SetBool("LF", false);
+                playerAnimator.SetBool("RB", false);
+                playerAnimator.SetBool("LB", true);
+                playerStatus = PlayerStatus.LB;
+            }
+
+            operation += 1;
         }
         else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             x = 0;
             y = -3;
-            playerAnimator.SetBool("Forward", true);
-            playerAnimator.SetBool("Back", false);
-            playerAnimator.SetBool("Right", false);
-            playerAnimator.SetBool("Left", false);
+
+            if (playerStatus == PlayerStatus.RB)
+            {
+                playerAnimator.SetBool("RF", true);
+                playerAnimator.SetBool("LF", false);
+                playerAnimator.SetBool("RB", false);
+                playerAnimator.SetBool("LB", false);
+                playerStatus = PlayerStatus.RF;
+            }
+
+            if (playerStatus == PlayerStatus.LB)
+            {
+                playerAnimator.SetBool("RF", false);
+                playerAnimator.SetBool("LF", true);
+                playerAnimator.SetBool("RB", false);
+                playerAnimator.SetBool("LB", false);
+                playerStatus = PlayerStatus.LF;
+            }
+
+            operation += 1;
         }
         else if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
             x = -3;
             y = 0;
-            playerAnimator.SetBool("Left", true);
-            playerAnimator.SetBool("Forward", false);
-            playerAnimator.SetBool("Back", false);
-            playerAnimator.SetBool("Right", false);
+
+            if (playerStatus == PlayerStatus.RF)
+            {
+                playerAnimator.SetBool("RF", false);
+                playerAnimator.SetBool("LF", true);
+                playerAnimator.SetBool("RB", false);
+                playerAnimator.SetBool("LB", false);
+                playerStatus = PlayerStatus.LF;
+            }
+
+            if (playerStatus == PlayerStatus.RB)
+            {
+                playerAnimator.SetBool("RF", false);
+                playerAnimator.SetBool("LF", false);
+                playerAnimator.SetBool("RB", false);
+                playerAnimator.SetBool("LB", true);
+                playerStatus = PlayerStatus.LB;
+            }
+
+            operation += 1;
         }
         else if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             x = 3;
             y = 0;
-            playerAnimator.SetBool("Right", true);
-            playerAnimator.SetBool("Left", false);
-            playerAnimator.SetBool("Forward", false);
-            playerAnimator.SetBool("Back", false);
+
+            if (playerStatus == PlayerStatus.LB)
+            {
+                playerAnimator.SetBool("RF", false);
+                playerAnimator.SetBool("LF", false);
+                playerAnimator.SetBool("RB", true);
+                playerAnimator.SetBool("LB", false);
+                playerStatus = PlayerStatus.RB;
+            }
+
+            if (playerStatus == PlayerStatus.LF)
+            {
+                playerAnimator.SetBool("RF", true);
+                playerAnimator.SetBool("LF", false);
+                playerAnimator.SetBool("RB", false);
+                playerAnimator.SetBool("LB", false);
+                playerStatus = PlayerStatus.RF;
+            }
+
+            operation += 1;
         }
         moveDirection.x = x;
         moveDirection.y = y;
+        rb.velocity = Vector3.zero;
 
         transform.position += moveDirection * Time.deltaTime;
         transform.rotation = Quaternion.identity;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // ゴールしたら
+        if(collision.gameObject.tag == "Goal")
+        {
+            resultManager.ResultStart();
+            Time.timeScale = 0;
+        }
+
+        // 何かにぶつかったら
+        if(collision.gameObject.tag == "Object")
+        {
+            Debug.Log("Hit!");
+        }
     }
 }
---
+++ Kazehahuku/Assets/Scripts/ResultManager.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResultManager : MonoBehaviour
+{
+    //[SerializeField] Image starImage;
+    [SerializeField] SpriteRenderer resultFrame;
+    [SerializeField] Text result;
+    [SerializeField] Text clear;
+    [SerializeField] Text clearNumber;
+    [SerializeField] Text damage;
+    [SerializeField] Text damageNumber;
+    [SerializeField] Text operation;
+    [SerializeField] Text operationNumber;
+    [SerializeField] Text evaluation;
+    [SerializeField] GameObject star1;
+    [SerializeField] GameObject star2;
+    [SerializeField] GameObject star3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //resultFrame.color -= new Color(0, 0, 0, 0.01f);
+    }
+
+    public void ResultStart()
+    {
+        iTween.ValueTo(gameObject, iTween.Hash("from", 0, "to", 1, "time", 1.5f, "delay", 0, "onupdate", "FadeinFrame"));
+        iTween.ValueTo(gameObject, iTween.Hash("from", 0, "to", 1, "time", 0.5f, "delay", 1.5, "onupdate", "ResultText"));
+        iTween.ValueTo(gameObject, iTween.Hash("from", 0, "to", 1, "time", 0.5f, "delay", 2, "onupdate", "ClearTime"));
+        iTween.ValueTo(gameObject, iTween.Hash("from", 0, "to", 1, "time", 0.5f, "delay", 2.5, "onupdate", "ClearTimeNumber"));
+        iTween.ValueTo(gameObject, iTween.Hash("from", 0, "to", 1, "time", 0.5f, "delay", 3, "onupdate", "Damage"));
+        iTween.ValueTo(gameObject, iTween.Hash("from", 0, "to", 1, "time", 0.5f, "delay", 3.5, "onupdate", "DamageNumber"));
+        iTween.ValueTo(gameObject, iTween.Hash("from", 0, "to", 1, "time", 0.5f, "delay", 4, "onupdate", "Operation"));
+        iTween.ValueTo(gameObject, iTween.Hash("from", 0, "to", 1, "time", 0.5f, "delay", 4.5, "onupdate", "OperationNumber"));
+        iTween.ValueTo(gameObject, iTween.Hash("from", 0, "to", 1, "time", 0.5f, "delay", 5, "onupdate", "Evaluation"));
+        //iTween.ValueTo(gameObject, iTween.Hash("from", 0, "to", 1, "time", 0.5f, "delay", 5.5, "onupdate", ""));
+
+        iTween.ValueTo(gameObject, iTween.Hash("from", 0, "to", 360, "time", 0.5f, "delay", 5.5, "onupdate", "RotateStar1"));
+        iTween.ValueTo(gameObject, iTween.Hash("from", 0.1, "to", 0.05, "time", 0.5f, "delay", 5.5, "onupdate", "MoveStar1"));
+        iTween.ValueTo(gameObject, iTween.Hash("from", 0, "to", 360, "time", 0.5f, "delay", 6, "onupdate", "RotateStar2"));
+        iTween.ValueTo(gameObject, iTween.Hash("from", 0.1, "to", 0.05, "time", 0.5f, "delay", 6, "onupdate", "MoveStar2"));
+        iTween.ValueTo(gameObject, iTween.Hash("from", 0, "to", 360, "time", 0.5f, "delay", 6.5, "onupdate", "RotateStar3"));
+        iTween.ValueTo(gameObject, iTween.Hash("from", 0.1, "to", 0.05, "time", 0.5f, "delay", 6.5, "onupdate", "MoveStar3"));
+    }
+
+    void FadeinFrame(float value)
+    {
+        resultFrame.color = new Color(1, 1, 1, value);
+    }
+
+    void ResultText(float value)
+    {
+        result.color = new Color(0, 0, 0, value);
+    }
+
+    void ClearTime(float value)
+    {
+        clear.color = new Color(0, 0, 0, value);
+    }
+
+    void ClearTimeNumber(float value)
+    {
+        clearNumber.color = new Color(0, 0, 0, value);
+    }
+
+    void Damage(float value)
+    {
+        damage.color = new Color(0, 0, 0, value);
+    }
+
+    void DamageNumber(float value)
+    {
+        damageNumber.color = new Color(0, 0, 0, value);
+    }
+
+    void Operation(float value)
+    {
+        operation.color = new Color(0, 0, 0, value);
+    }
+
+    void OperationNumber(float value)
+    {
+        operationNumber.color = new Color(0, 0, 0, value);
+    }
+
+    void Evaluation(float value)
+    {
+        evaluation.color = new Color(0, 0, 0, value);
+    }
+
+    void RotateStar1(float value)
+    {
+        star1.SetActive(true);
+        star1.transform.rotation = Quaternion.Euler(0, 0, value);
+    }
+
+    void MoveStar1(float value)
+    {
+        star1.transform.localScale = new Vector3(value, value, value);
+    }
+
+    void RotateStar2(float value)
+    {
+        star2.SetActive(true);
+        star2.transform.rotation = Quaternion.Euler(0, 0, value);
+    }
+
+    void MoveStar2(float value)
+    {
+        star2.transform.localScale = new Vector3(value, value, value);
+    }
+
+    void RotateStar3(float value)
+    {
+        star3.SetActive(true);
+        star3.transform.rotation = Quaternion.Euler(0, 0, value);
+    }
+
+    void MoveStar3(float value)
+    {
+        star3.transform.localScale = new Vector3(value, value, value);
+    }
+}
