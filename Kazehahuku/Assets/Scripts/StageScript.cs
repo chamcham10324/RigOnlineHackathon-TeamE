ee6a2891937afe2432b691bae13c34c1c39177aa
chamcham3 <is0456fx@ed.ritsumei.ac.jp>
2020-09-12 00:13:54 +0900

    Add Stage Select Scene(nakao)
    中尾くんが作ってくれたステージセレクトシーンを追加しました。

---
+++ Kazehahuku/Assets/Scripts/StageScript.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StageScript : MonoBehaviour
+{
+    public Thumbnail TS;
+    public int register;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void MouseOver() 
+    {
+        TS.Flag = register;
+    }
+
+    public void SelectStage() {
+        SceneManager.LoadScene("Stage" +register);
+    }
+}
---
+++ Kazehahuku/Assets/Scripts/Thumbnail.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Thumbnail : MonoBehaviour
+{
+    public int Flag;
+    int OldFlag;
+
+    Image MainSpriteRenderer;
+    // publicで宣言し、inspectorで設定可能にする
+    public Sprite Stage1;
+    public Sprite Stage2;
+    public Sprite Stage3;
+    public Sprite Stage4;
+    public Sprite Stage5;
+
+    void Start()
+    {
+        Flag = 0;
+        OldFlag = 0;
+        // このobjectのSpriteRendererを取得
+        MainSpriteRenderer = gameObject.GetComponent<Image>();
+    }
+
+    void Update() {
+        if (OldFlag != Flag) {
+            if (Flag == 1) MouseOnStage1();
+            else if (Flag == 2) MouseOnStage2();
+            else if (Flag == 3) MouseOnStage3();
+            else if (Flag == 4) MouseOnStage4();
+            else if (Flag == 5) MouseOnStage5();
+
+            OldFlag = Flag;
+        }
+    }
+
+    void MouseOnStage1()
+    {
+        MainSpriteRenderer.sprite = Stage1;
+    }
+    void MouseOnStage2()
+    {
+        MainSpriteRenderer.sprite = Stage2;
+    }
+    void MouseOnStage3()
+    {
+        MainSpriteRenderer.sprite = Stage3;
+    }
+    void MouseOnStage4()
+    {
+        MainSpriteRenderer.sprite = Stage4;
+    }
+    void MouseOnStage5()
+    {
+        MainSpriteRenderer.sprite = Stage5;
+    }
+}
